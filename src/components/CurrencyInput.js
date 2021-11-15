441cca6dc8e1bc912d9624f36738b3f3777e377e
braveheart-star <mouse1996115@gmail.com>
2021-11-15 12:57:25 +0800

    currency implemented

--- src/components/CurrencyInput.js
+++ src/components/CurrencyInput.js
@@ -2,20 +2,20 @@ import React from "react";
 import MaskedInput from "react-text-mask";
 import createNumberMask from "text-mask-addons/dist/createNumberMask";
 
-const defaultMaskOptions = {
-  prefix: "$",
-  suffix: "",
-  includeThousandsSeparator: true,
-  thousandsSeparatorSymbol: ",",
-  allowDecimal: true,
-  decimalSymbol: ".",
-  decimalLimit: 2, // how many digits allowed after the decimal
-  integerLimit: 7, // limit length of integer numbers
-  allowNegative: false,
-  allowLeadingZeroes: false,
-};
+const CurrencyInput = ({ maskOptions, prefix, ...inputProps }) => {
+  const defaultMaskOptions = {
+    prefix: prefix,
+    suffix: "",
+    includeThousandsSeparator: true,
+    thousandsSeparatorSymbol: ",",
+    allowDecimal: true,
+    decimalSymbol: ".",
+    decimalLimit: 2, // how many digits allowed after the decimal
+    integerLimit: 7, // limit length of integer numbers
+    allowNegative: false,
+    allowLeadingZeroes: false,
+  };
 
-const CurrencyInput = ({ maskOptions, ...inputProps }) => {
   const currencyMask = createNumberMask({
     ...defaultMaskOptions,
     ...maskOptions,
--- src/components/ExchangeModal.js
+++ src/components/ExchangeModal.js
@@ -1,16 +1,108 @@
-import React from "react";
+import axios from "axios";
 import Select from "react-select";
+import React, { useEffect, useState } from "react";
 import CurrencyInput from "./CurrencyInput";
-
+import { convertRateMock } from "../utils/mockData";
 import Logo from "../assets/logo.png";
 import "./index.scss";
 const ExchangeModal = () => {
   const options = [
+    { value: "USD", label: "USD" },
     { value: "EUR", label: "EUR" },
     { value: "GBP", label: "GBP" },
-    { value: "USD", label: "USD" },
   ];
 
+  const [reduceWallet, setReduceWallet] = useState(options[0]);
+  const [increaseWallet, setIncreaseWallet] = useState(options[1]);
+  const [convertRate, setConvertRate] = useState(convertRateMock);
+  const [increaseAmount, setIncreaseAmount] = useState();
+  const [reduceAmount, setReduceAmount] = useState();
+  const [increaseError, setIncreaseError] = useState(false);
+  const [reduceError, setReduceError] = useState(false);
+
+  const [currencyBalance, setCurrencyBalance] = useState({
+    USD: 200,
+    EUR: 150,
+    GBP: 10,
+  });
+
+  const currencySymbol = {
+    USD: "$",
+    EUR: "€",
+    GBP: "£",
+  };
+
+  const currencyConvert = {
+    USD: 1,
+    EUR: convertRate["EUR"],
+    GBP: convertRate["GBP"],
+  };
+
+  async function fetchConvertRate() {
+    await axios({
+      url: "https://freecurrencyapi.net/api/v2/latest?apikey=98af3480-426a-11ec-a903-2fddc33818b2",
+      method: "GET",
+    }).then((res) => {
+      setConvertRate(res.data.data);
+    });
+  }
+  // useEffect(() => {
+  //   fetchConvertRate();
+  // }, []);
+
+  useEffect(() => {
+    if (reduceAmount) {
+      console.log(
+        "rate ----> ",
+        currencyConvert[reduceWallet.value] /
+          currencyConvert[increaseWallet.value]
+      );
+      setIncreaseAmount(
+        (Number(reduceAmount.slice(1)) *
+          currencyConvert[increaseWallet.value]) /
+          currencyConvert[reduceWallet.value]
+      );
+    }
+  }, [reduceWallet, increaseWallet]);
+
+  const handleConvertCurrency = () => {
+    const reduceWalletCurrency = reduceWallet.value;
+    const increaseWalletCurrency = increaseWallet.value;
+
+    // currencyBalance[reduceWalletCurrency] =
+    //   currencyBalance[reduceWalletCurrency] -
+    //   (isNaN(reduceAmount) ? Number(reduceAmount.slice(1)) : reduceAmount);
+    let calculatedReduceCurrency =
+      currencyBalance[reduceWalletCurrency] -
+      (isNaN(reduceAmount) ? Number(reduceAmount.slice(1)) : reduceAmount);
+
+    let calculatedIncreaseCurrency =
+      currencyBalance[increaseWalletCurrency] +
+      (isNaN(increaseAmount)
+        ? Number(increaseAmount.slice(1))
+        : increaseAmount);
+
+    if (calculatedReduceCurrency < 0) {
+      setReduceError(true);
+      return;
+    } else {
+      currencyBalance[reduceWalletCurrency] = calculatedReduceCurrency;
+    }
+    if (calculatedIncreaseCurrency < 0) {
+      setIncreaseError(true);
+      return;
+    } else {
+      currencyBalance[increaseWalletCurrency] = calculatedIncreaseCurrency;
+    }
+    setIncreaseError(false);
+    setReduceError(false);
+    setIncreaseAmount(0);
+    setReduceAmount(0);
+
+    setCurrencyBalance({
+      ...currencyBalance,
+    });
+  };
   return (
     <div className="modal-container">
       <div className="modal-header">
@@ -18,35 +110,104 @@ const ExchangeModal = () => {
         <p className="ToDo-Header">Currency Exchange</p>
       </div>
       <div className="convert-wrapper">
+        <div className="conversion-part">
+          <div className="convert-modal">
+            {`${currencySymbol[reduceWallet.value]} 1.00 = ${
+              currencySymbol[increaseWallet.value]
+            } ${(
+              currencyConvert[increaseWallet.value] /
+              currencyConvert[reduceWallet.value]
+            ).toFixed(2)}`}
+          </div>
+        </div>
         <div className="reduce-part">
           <div className="currency-wrapper">
             <div className="currency-select">
-              <Select options={options} />
+              <Select
+                value={reduceWallet}
+                options={options}
+                onChange={setReduceWallet}
+              />
             </div>
             <div className="balance-wrapper">
-              <span>Balance :</span>
+              <span>
+                Balance : {currencySymbol[reduceWallet.value]}{" "}
+                {currencyBalance[reduceWallet.value]}
+              </span>
             </div>
           </div>
+          {reduceError && (
+            <span style={{ color: "red", right: "30px", position: "absolute" }}>
+              Exceeds Balance
+            </span>
+          )}
           <div className="currency-amount">
             <span className="symbol">-</span>
-            <CurrencyInput placeholder="$ 0.00" type="text" />
+
+            <CurrencyInput
+              placeholder={`${currencySymbol[reduceWallet.value]} 0.00`}
+              type="text"
+              prefix={currencySymbol[reduceWallet.value]}
+              onChange={(e) => {
+                setReduceAmount(e.target.value);
+                setIncreaseAmount(
+                  Number(
+                    (e.target.value.slice(1) *
+                      currencyConvert[increaseWallet.value]) /
+                      currencyConvert[reduceWallet.value]
+                  )
+                );
+              }}
+              value={reduceAmount}
+            />
           </div>
-        </div>
-        <div className="conversion-part">
-          <div className="convert-modal">hello</div>
         </div>
         <div className="increase-part">
-          <div className="currency-wrapper">
-            <div className="currency-select">
-              <Select options={options} />
+          <div className="increase-wrapper">
+            <div className="currency-wrapper">
+              <div className="currency-select">
+                <Select
+                  value={increaseWallet}
+                  options={options}
+                  onChange={setIncreaseWallet}
+                />
+              </div>
+              <div className="balance-wrapper">
+                <span>
+                  Balance : {currencySymbol[increaseWallet.value]}{" "}
+                  {currencyBalance[increaseWallet.value]}
+                </span>
+              </div>
             </div>
-            <div className="balance-wrapper">
-              <span>Balance :</span>
+            {increaseError && (
+              <span
+                style={{ color: "red", right: "30px", position: "absolute" }}
+              >
+                Exceeds Balance
+              </span>
+            )}
+            <div className="currency-amount">
+              <span className="symbol">+</span>
+              <CurrencyInput
+                placeholder={`${currencySymbol[increaseWallet.value]} 0.00`}
+                type="text"
+                prefix={currencySymbol[increaseWallet.value]}
+                onChange={(e) => {
+                  setIncreaseAmount(e.target.value);
+                  setReduceAmount(
+                    Number(
+                      (e.target.value.slice(1) *
+                        currencyConvert[reduceWallet.value]) /
+                        currencyConvert[increaseWallet.value]
+                    )
+                  );
+                }}
+                value={increaseAmount}
+              />
             </div>
           </div>
-          <div className="currency-amount">
-            <span className="symbol">+</span>
-            <CurrencyInput placeholder="$ 0.00" type="text" />
+          <div className="submit-button">
+            <button onClick={handleConvertCurrency}>Exchange</button>
           </div>
         </div>
       </div>
